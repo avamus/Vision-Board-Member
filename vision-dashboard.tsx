0031b6d3e23904547eb98b78638c9d08358982e2
avamus <lukassys2004@gmail.com>
2025-02-17 21:48:13 +0100

    Create vision-dashboard.tsx

---
+++ vision-dashboard.tsx
@@ -0,0 +1,22 @@
+import { MainVisionPanel } from "./main-vision-panel"
+import { ChallengeCard } from "./challenge-card"
+import { ProgressWinsSection } from "./progress-wins-section"
+import { ImageGallery } from "./image-gallery"
+import { TeamGoalsVision } from "./team-goals-vision"
+
+export function VisionDashboard() {
+  return (
+    <div className="container mx-auto p-6">
+      <div className="flex flex-col gap-6 rounded-xl">
+        <TeamGoalsVision />
+        <MainVisionPanel />
+        <div className="grid gap-6 md:grid-cols-2">
+          <ChallengeCard />
+          <ProgressWinsSection />
+        </div>
+        <ImageGallery />
+      </div>
+    </div>
+  )
+}
+
